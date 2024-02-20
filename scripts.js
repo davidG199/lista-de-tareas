9da59eb6a95befbbdbf7029e39703a16dc89aaef
David Gomez <david.gomez04@hotmail.com>
2024-02-19 22:03:04 -0500

    First commit

---
+++ scripts.js
@@ -0,0 +1,54 @@
+const input = document.getElementById("ingresar-tarea");
+const boton = document.querySelector('button');
+const listaDeTarea = document.getElementById("lista-de-tareas");
+
+function agregarTarea(){
+
+    if (input.value) {
+        let tareaNueva = document.createElement('div')
+        tareaNueva.classList.add('tarea')
+
+        let texto = document.createElement('p')
+        texto.innerText = input.value;
+        tareaNueva.appendChild(texto);
+
+        let iconos = document.createElement('div')
+        iconos.classList.add('iconos')
+        tareaNueva.appendChild(iconos);
+
+        let completar = document.createElement('i')
+        completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar')
+        completar.addEventListener('click', completarTarea);
+
+        let eliminar = document.createElement('i')
+        eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar')
+        eliminar.addEventListener('click', borrarTarea);
+
+        iconos.append(completar, eliminar)
+
+        listaDeTarea.appendChild(tareaNueva)
+    } else{
+        alert('por favor ingresa una tarea')
+    }
+}
+
+function completarTarea(e) {
+    let tarea = e.target.parentNode.parentNode;
+    tarea.classList.toggle('completada')
+}
+
+function borrarTarea(e) {
+    let tarea = e.target.parentNode.parentNode;
+    tarea.remove();
+}
+
+boton.addEventListener('click', agregarTarea);
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        agregarTarea();
+    }
+});
+
+
+
